23a9771742e8fef8896041a1ec169c48815dbb72
carlos.beltran-at-674969126656 <canti89@gmail.com>
2018-05-09 12:53:35 -0500

    113 Claculating the ingredients sum Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,22 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
 
-    const tranformedIngredients = Object.keys(props.ingredients)
+    let tranformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])]
                     .map((_, i) => {
                         return <BurgerIngredient key={igKey + i} type={igKey} />
                     })
         })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
+    
+    if (tranformedIngredients.length === 0) {
+        tranformedIngredients = (
+            <p>Please start adding ingredients!</p>
+        )
+    }
 
     return (
         <div className={classes.Burger}>
