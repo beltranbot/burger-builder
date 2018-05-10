b1fd405940ee3fb3cd128d713e0a047c6bf36fb5
haitchex <canti89@gmail.com>
2018-05-09 22:13:34 -0500

    120 Creating the Order Summary Modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Aux from "../../../hoc/Auxiliary"
+
+const orderSumary = (props) => {
+    const capitalize = {capitalize: 'capitalize'}
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => (
+            <li key={igKey}>
+                <span style={capitalize}>
+                    {igKey}
+                </span>: {props.ingredients[igKey]}
+            </li>
+        ))
+
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicuous burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Aux>
+    )
+}
+
+export default orderSumary
