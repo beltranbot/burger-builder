255993341d98c4b1cca8b00547d5ea2921f8fb76
haitchex <canti89@gmail.com>
2018-05-14 08:33:17 -0500

    209 Adding an Orders Page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Order.css'
+
+const order = (props) => (
+    <div>
+        <p>Ingredients: Salad (1)</p>
+        <p>Price: 5.45</p>
+    </div>
+)
+
+export default order
