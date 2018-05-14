f01280b16473e07e087172b8689e40cbaf7428ea
haitchex <canti89@gmail.com>
2018-05-14 16:57:04 -0500

    230 Wrap Up

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,6 +85,7 @@ class ContactData extends Component {
                     ]
                 },
                 value: 'fastest',
+                validation: {},
                 valid: true
             },
         },
@@ -120,6 +121,7 @@ class ContactData extends Component {
 
     checkValidity (value, rules) {
         let isValid = true
+        if (!rules) return true
         if (rules.required)
             isValid = (value.trim() !== '') && isValid
         if (rules.minLength)
