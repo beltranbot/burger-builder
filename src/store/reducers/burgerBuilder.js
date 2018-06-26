832db41c3f19917678e3b594ab2f9ad9bec09963
beltranbot@gmail.com <canti89@gmail.com>
2018-06-25 20:14:44 -0500

    294 Resetting the Price after Purchases

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -38,6 +38,7 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 ingredients: action.ingredients,
+                totalPrice: 4,
                 error: true
             }
         case actionTypes.FETCH_INGREDIENTS_FAILED:
