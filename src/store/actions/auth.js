d7b17f107defd7d5d96da117de51c18fbd7a8d2f
beltranbot@gmail.com <canti89@gmail.com>
2018-06-30 09:56:04 -0500

    305 Adding Actions

---
+++ src/store/actions/auth.js
@@ -0,0 +1,28 @@
+import * as actionTypes from './actionTypes'
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    }
+}
+
+export const authSucess = (authData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData
+    }
+}
+
+export const authFailed = (error) => {
+    return {
+        type: actionTypes.AUTH_FAILED,
+        error
+    }
+}
+
+export const auth = (email, password) => {
+    return dispatch => {
+        // .. 
+        dispatch(authStart())
+    }
+}
