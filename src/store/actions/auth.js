9ee21ce13a4c278fa3c4d072c43d33da9f72a45c
beltranbot@gmail.com <canti89@gmail.com>
2018-07-01 07:38:36 -0500

    306 Getting a Token from the Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,6 @@
 import * as actionTypes from './actionTypes'
+import axios from 'axios'
+import {FIREBASE_API_KEY} from '../../env'
 
 export const authStart = () => {
     return {
@@ -22,7 +24,22 @@ export const authFailed = (error) => {
 
 export const auth = (email, password) => {
     return dispatch => {
-        // .. 
         dispatch(authStart())
+        const authData = {
+            email,
+            password,
+            returnSecureToken: true
+        }
+        axios.post(
+            `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${FIREBASE_API_KEY}`,
+            authData
+        ).then(response => {
+            console.log(response)
+            dispatch(authSucess(response.data))
+        })
+        .catch(error => {
+            console.log(error)
+            dispatch(authFailed(error))
+        })
     }
 }
